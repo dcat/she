0f6d11aef10ba99c65d13729c53487117bd021b3
dcat <dcat@iotek.org>
2018-04-22 20:59:03 +0200

    show diff

--- she.c
+++ she.c
@@ -16,12 +16,19 @@
 #define BG TB_DEFAULT
 #define PGSIZ 16
 
+enum {
+	HEX,
+	ASCII,
+};
+
 typedef struct {
 	char *map;
 	int d;
 	off_t siz;
 	off_t off;
 	off_t csr;
+	int mode;
+	char *orig;
 } he_t;
 
 static he_t he;
@@ -59,6 +66,7 @@ tb_printf(int x, int y, int fg, int bg, const char *fmt, ...) {
 void
 redraw(void) {
 	int y, x, z, j, i;
+	int fg, bg;
 
 	tb_clear();
 
@@ -71,23 +79,28 @@ redraw(void) {
 
 		for (x = 0; x < 4; x++) {
 			for (z = 0; z < 4; z++, i++) {
+				fg = he.map[i] == he.orig[i] ? FG : 196;
+
 				/* hex chars */
 				tb_printf(13 + (x * 12) + (z * 3), y,
-					i < he.siz ? FG : 239, BG, "%02x",
+					i < he.siz ? fg : 239, BG, "%02x",
 					i > he.siz ? 0 :
 					(unsigned char)he.map[i]);
 
 				/* ascii */
 				tb_printf(64 + i % 16, y, i >= he.siz ? 239
-					: FG, BG, "%c", isprint(he.map[i])
+					: fg, BG, "%c", isprint(he.map[i])
 					? he.map[i] : '.');
 
 				/* focused */
 				if (he.csr == i) {
 					tb_printf(13 + (x * 12) + (z * 3), y,
-						16, 255, "%02x",
+						16, he.mode == HEX ? 226 : 255,
+						"%02x",
 						(unsigned char)he.map[i]);
-					tb_printf(64 + i % 16, y, 16, 255, "%c",
+					tb_printf(64 + i % 16, y, 16,
+						he.mode == ASCII ? 226 : 225,
+						"%c",
 						isprint(he.map[i]) ? he.map[i]
 						: '.');
 				}
@@ -96,7 +109,8 @@ redraw(void) {
 	}
 
 	/* status bar */
-	tb_printf(0, tb_height() - 1, FG, BG, "^C quit, ^X save");
+	tb_printf(0, tb_height() - 1, FG, BG, "%s ^C quit, ^X save",
+			he.mode == ASCII ? "ascii" : "hex");
 	tb_present();
 }
 
@@ -170,13 +184,23 @@ main(int argc, char **argv) {
 	if (he.map == MAP_FAILED)
 		goto error;
 
+	he.orig = malloc(sizeof(char) * he.siz);
+	if (he.orig == NULL)
+		goto error;
+
+	memcpy(he.orig, he.map, he.siz);
+
 	he.off = he.csr = 0;
+	he.mode = HEX;
 	redraw();
 
 	while (tb_poll_event(&ev) > 0) {
 		switch (ev.type) {
 		case TB_EVENT_KEY:
 			switch (ev.key) {
+			case TB_KEY_TAB:
+				he.mode = he.mode == ASCII ? HEX : ASCII;
+				break;
 			case TB_KEY_CTRL_C:
 				goto end;
 				/* NOTREACHED */
@@ -186,6 +210,7 @@ main(int argc, char **argv) {
 			case TB_KEY_CTRL_X:
 				lseek(he.d, 0, 0);
 				write(he.d, he.map, he.siz);
+				memcpy(he.orig, he.map, he.siz);
 				break;
 			case TB_KEY_CTRL_D:
 			case TB_KEY_PGDN:
@@ -211,53 +236,57 @@ main(int argc, char **argv) {
 				scroll(ev.key);
 				break;
 			default:
-				switch (ev.ch) {
-				case 'g':
-					if (tb_poll_event(&ev) != TB_EVENT_KEY)
-						break;
+				if (he.mode == HEX) {
+					switch (ev.ch) {
+					case 'g':
+						if (tb_poll_event(&ev) != TB_EVENT_KEY)
+							break;
 
-					if (ev.ch == 'g')
-						scroll(TB_KEY_HOME);
-					break;
-				case 'G':
-					scroll(TB_KEY_END);
-					break;
-				case 'a': case 'A':
-				case 'b': case 'B':
-				case 'c': case 'C':
-				case 'd': case 'D':
-				case 'e': case 'E':
-				case 'f': case 'F':
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9': {
-					unsigned char s1, s2;
-					char p[3] = { 0, 0, 0 };
-					s1 = ev.ch;
-					tb_printf(13 + (he.csr % 16) * 3,
-						(he.csr - he.off) / 16,
-						16, 255, "%c_", ev.ch);
-					tb_present();
-					if (tb_poll_event(&ev) != TB_EVENT_KEY)
+						if (ev.ch == 'g')
+							scroll(TB_KEY_HOME);
 						break;
+					case 'G':
+						scroll(TB_KEY_END);
+						break;
+					case 'a': case 'A':
+					case 'b': case 'B':
+					case 'c': case 'C':
+					case 'd': case 'D':
+					case 'e': case 'E':
+					case 'f': case 'F':
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9': {
+						unsigned char s1, s2;
+						char p[3] = { 0, 0, 0 };
+						s1 = ev.ch;
+						tb_printf(13 + (he.csr % 16) * 3,
+							(he.csr - he.off) / 16,
+							16, 255, "%c_", ev.ch);
+						tb_present();
+						if (tb_poll_event(&ev) != TB_EVENT_KEY)
+							break;
 
-					s2 = ev.ch;
-					p[0] = s1;
-					p[1] = s2;
-					he.map[he.csr] = (unsigned char)
-						strtoul(p, NULL, 16);
-					/* FALLTHROUGH */
-				}
-				default:
-					redraw();
-					break;
+						s2 = ev.ch;
+						p[0] = s1;
+						p[1] = s2;
+						he.map[he.csr] = (unsigned char)
+							strtoul(p, NULL, 16);
+						/* FALLTHROUGH */
+					}
+					default:
+						redraw();
+						break;
+					}
+				} else if (he.mode == ASCII) {
+					he.map[he.csr] = ev.ch;
 				}
 			}
 		case TB_EVENT_RESIZE:
