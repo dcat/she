75936724da272ed2f9f1823bd87bdeabd2e113d0
dcat <dcat@iotek.org>
2018-05-17 15:44:35 +0200

    scroll to offset

--- she.c
+++ she.c
@@ -31,9 +31,37 @@ typedef struct {
 	char *orig;
 	int insert;
 	int status;
+	int has_err;
 } he_t;
 
 static he_t he;
+
+int
+is_modified() {
+	int change, i;
+
+	for (i = 0; i < he.siz; i++) {
+		if (he.map[i] != he.orig[i]) {
+			change = 1;
+			break;
+		}
+	}
+
+	return change;
+
+}
+
+void
+cleanup(void) {
+	tb_shutdown();
+	munmap(he.map, he.siz);
+	close(he.d);
+
+	if (he.has_err && errno)
+		warn("error");
+
+	exit(0);
+}
 
 size_t
 search(const void *l, size_t l_len, const void *s, size_t s_len)
@@ -132,11 +160,26 @@ redraw(void) {
 	he.status = 0;
 }
 
+int
+isxdigits(char *str) {
+	char *p = str;
+
+	while (*p) {
+		if (isxdigit(*p))
+			*p++;
+		else
+			return 0;
+	}
+
+	return 1;
+}
+
 void
 scroll(uint16_t key) {
 	int i;
 
 	switch (key) {
+	case 'k':
 	case TB_KEY_ARROW_UP:
 		if (he.csr >= 16) {
 			if ((he.csr - he.off) / 16 == 0)
@@ -145,6 +188,7 @@ scroll(uint16_t key) {
 			he.csr -= 16;
 		}
 		break;
+	case 'j':
 	case TB_KEY_ARROW_DOWN:
 		if ((he.csr - he.off) / 16 == tb_height() - 2)
 			he.off += 16;
@@ -152,6 +196,7 @@ scroll(uint16_t key) {
 		if (he.csr < he.siz - 16)
 			he.csr += 16;
 		break;
+	case 'l':
 	case TB_KEY_ARROW_RIGHT:
 		if ((he.csr - he.off) / 16 == tb_height() - 2)
 			if ((he.csr % 16) == 15)
@@ -160,6 +205,7 @@ scroll(uint16_t key) {
 		if (he.csr < he.siz - 1)
 			he.csr += 1;
 		break;
+	case 'h':
 	case TB_KEY_ARROW_LEFT:
 		if (he.csr > 0)
 			he.csr -= 1;
@@ -175,10 +221,45 @@ scroll(uint16_t key) {
 	}
 }
 
+void
+runcmd(char *cmd) {
+	struct tb_event ev;
+	ssize_t offset;
+	int i;
+
+	switch (cmd[0]) {
+	case 'q':
+		if (cmd[1] == 0) {
+			if (is_modified()) {
+				tb_printf(0, tb_height() - 1, FG, BG,
+					"you have unsaved changes, press ^C again to quit");
+				tb_present();
+				if (tb_poll_event(&ev) != TB_EVENT_KEY)
+					break;
+				if (ev.key == TB_KEY_CTRL_C)
+					cleanup();
+			} else
+				cleanup();
+			break;
+		}
+	}
+
+	if (isxdigits(cmd)) {
+		offset = strtoul(cmd, NULL, 16);
+
+		scroll(TB_KEY_HOME);
+		for (i = 0; i < offset / 16; i++)
+			scroll(TB_KEY_ARROW_DOWN);
+	}
+
+	return;
+}
+
 int
 main(int argc, char **argv) {
 	struct tb_event ev;
-	int i, has_err, change;
+	int i, change;
+	char needle[256];
 
 	setlocale(LC_ALL, "");
 	tb_init();
@@ -212,6 +293,8 @@ main(int argc, char **argv) {
 	he.mode = HEX;
 	redraw();
 
+					char *p = needle;
+					size_t base;
 	while (tb_poll_event(&ev) > 0) {
 		switch (ev.type) {
 		case TB_EVENT_KEY:
@@ -220,23 +303,16 @@ main(int argc, char **argv) {
 				he.mode = he.mode == ASCII ? HEX : ASCII;
 				break;
 			case TB_KEY_CTRL_C:
-				for (i = 0; i < he.siz; i++) {
-					if (he.map[i] != he.orig[i]) {
-						change = 1;
-						break;
-					}
-				}
-
-				if (change) {
+				if (is_modified()) {
 					tb_printf(0, tb_height() - 1, FG, BG,
 						"you have unsaved changes, press ^C again to quit");
 					tb_present();
 					if (tb_poll_event(&ev) != TB_EVENT_KEY)
 						break;
 					if (ev.key == TB_KEY_CTRL_C)
-						goto end;
+						cleanup();
 				} else
-					goto end;
+					cleanup();
 				/* NOTREACHED */
 			case TB_KEY_CTRL_L:
 				redraw();
@@ -322,6 +398,47 @@ main(int argc, char **argv) {
 				}
 
 				switch (ev.ch) {
+				case ':': {
+					/* command */
+					char cmdbuf[BUFSIZ];
+					char *p = cmdbuf;
+
+					memset(p, 0, BUFSIZ);
+					tb_printf(0, tb_height() - 1,
+							16, 255, "%-*c",
+							tb_width(), ':');
+					tb_present();
+
+					he.status = 1;
+					redraw();
+
+					while (ev.key != TB_KEY_ESC) {
+						tb_poll_event(&ev);
+						*p++ = ev.ch;
+
+						/* handle backspace? */
+
+						if (ev.key == TB_KEY_ENTER) {
+							/* run command */
+							runcmd(cmdbuf);
+							break;
+						}
+
+						tb_printf(0, tb_height() - 1,
+							16, 255, ":%-*s",
+							tb_width() - 1, cmdbuf);
+						tb_present();
+					}
+
+					he.status = 0;
+
+				}	break;
+				case 'h':
+				case 'j':
+				case 'k':
+				case 'l':
+					scroll(ev.ch);
+					break;
 				case 'g':
 					if (tb_poll_event(&ev) != TB_EVENT_KEY)
 						break;
@@ -335,11 +452,26 @@ main(int argc, char **argv) {
 				case 'i':
 					he.insert = 1;
 					break;
-				case '/': {
-					char needle[256];
-					char *p = needle;
-					size_t base;
+				case 'n':
+					base = search(he.map + he.csr + 1,
+						(he.siz - he.csr) - 1, needle,
+						(p - needle) - 1);
 
+					if (base == 0) {
+						tb_printf(0, tb_height() - 1,
+							16, 196, "not found");
+						tb_present();
+						he.status = 1;
+					} else {
+						scroll(TB_KEY_HOME);
+						he.off = base / 16;
+						for (i = 0; i < he.off; i++)
+							scroll(TB_KEY_ARROW_DOWN);
+						he.csr = base;
+						redraw();
+					}
+					break;
+				case '/': {
 					memset(needle, 0, 256);
 
 					while (ev.key != TB_KEY_ENTER) {
@@ -376,15 +508,8 @@ main(int argc, char **argv) {
 	}
 
 error:
-	has_err = 1;
-end:
-	tb_shutdown();
-	munmap(he.map, he.siz);
-	close(he.d);
-
-	if (has_err && errno)
-		warn("error");
-
+	he.has_err = 1;
+	cleanup();
 	return 0;
 }
 
